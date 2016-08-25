66a05c6f29dd266c54dcb7335b6f2d4b1274ee6b
Sudarshan Sangwan <sudsangwan@users.noreply.github.com>
2016-08-25 18:34:09 +0530

    Update ViewHolder.java
    Vikas Work commit

--- app/src/main/java/com/example/sud/sampleassignment/ViewHolder.java
+++ app/src/main/java/com/example/sud/sampleassignment/ViewHolder.java
@@ -1,5 +1,5 @@
 package com.example.sud.sampleassignment;
-
+//TODO : Change the package name and you good to go
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -11,14 +11,16 @@ import android.widget.TextView;
  * Created by Sudarshan on 05/04/16.
  */
 public class ViewHolder extends RecyclerView.ViewHolder {
-
+    
+    //TODO : Give these variables accordingly to your work but keep last three
     TextView name;
     TextView price;
     Spinner spinner;
     ViewPager viewPager;
     ImageView leftArrow;
     ImageView rightArrow;
-
+    
+    //TODO : Change accordingly from above 
     ViewHolder(View itemView) {
         super(itemView);
         name = (TextView) itemView.findViewById(R.id.tvName);
